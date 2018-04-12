f6c9b3b6e9edc84bc2c63ac3b970cca49c332f15
a.kim <alexander.kim.61@gmail.com>
2018-04-12 15:30:26 +0600

    Removed Realm

--- app/src/main/java/aleksandrkim/yandextestprep/YandexTestPrepApp.java
+++ app/src/main/java/aleksandrkim/yandextestprep/YandexTestPrepApp.java
@@ -2,9 +2,6 @@ package aleksandrkim.yandextestprep;
 
 import android.app.Application;
 
-import io.realm.Realm;
-import io.realm.RealmConfiguration;
-
 /**
  * Created by Aleksandr Kim on 08 Apr, 2018 12:06 AM for YandexTestPrep
  */
@@ -14,10 +11,5 @@ public class YandexTestPrepApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Realm.init(this);
-        Realm.setDefaultConfiguration(
-                new RealmConfiguration.Builder()
-                        .schemaVersion(BuildConfig.VERSION_CODE)
-                        .build());
     }
 }
